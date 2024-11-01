d8a8fdc7f18c292c8e5c4d75a3f1184e895b3906
Hashem Ahmed <122758455+hashem03x@users.noreply.github.com>
2024-11-01 22:56:31 +0200

    fix ts Errors

--- src/pages/tasks/List.tsx
+++ src/pages/tasks/List.tsx
@@ -9,13 +9,15 @@ import KanbanColumn from "@/components/tasks/kanban/KanbanColumn";
 import KanbanItem from "@/components/tasks/kanban/KanbanItem";
 import { UPDATE_TASK_STAGE_MUTATION } from "@/graphql/mutations";
 import { TASK_STAGES_QUERY, TASKS_QUERY } from "@/graphql/queries";
-import { TaskStage } from "@/graphql/schema.types";
-import { TasksQuery } from "@/graphql/types";
+import { TasksQuery, TaskStagesQuery } from "@/graphql/types";
 import { DragEndEvent } from "@dnd-kit/core";
 import { useList, useNavigation, useUpdate } from "@refinedev/core";
 import { GetFieldsFromList } from "@refinedev/nestjs-query";
 import React, { useMemo } from "react";
 
+type Task = GetFieldsFromList<TasksQuery>;
+
+type TaskStage = GetFieldsFromList<TaskStagesQuery> & { tasks: Task[] };
 function List({ children }: React.PropsWithChildren) {
   const { data: stages, isLoading: isLoadingStages } = useList<TaskStage>({
     resource: "taskStages",
